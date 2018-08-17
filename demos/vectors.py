bc907c25b0dd36ef8f985b4c3e47eb1d8c3a6e6a
Ashton Shears <ashears@uoregon.edu>
2018-08-17 15:45:39 -0700

    changed

--- demos/vectors.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import mpld3
-
-V = np.array([[1,1],[-2,2],[4,-7]])
-origin = [0], [0] # origin point
-
-plt.quiver(*origin, V[:,0], V[:,1], color=['r','b','g'], scale=21)
-mpld3.show()
